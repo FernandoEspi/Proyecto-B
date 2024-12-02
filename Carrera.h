37edc2486960a4e3732b0b1d0681a4e01eb3e301
FernandoEspi <166545241+FernandoEspi@users.noreply.github.com>
2024-12-02 17:02:45 -0500

    Create Carrera.h

---
+++ Carrera.h
@@ -0,0 +1,35 @@
+#ifndef CARRERA_H
+#define CARRERA_H
+
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Caballo {
+private:
+    string nombre;
+    int recorrido;
+    int pasos;
+
+public:
+    Caballo(string nombre);
+    ~Caballo();
+    void avanzar();
+    int getRecorrido() const;
+    int getPasos() const;
+    string getNombre() const;
+};
+
+class Carrera {
+private:
+    vector<Caballo> caballos;
+
+public:
+    Carrera(const vector<string>& nombres);
+    ~Carrera();
+    void simularCarrera();
+    void mostrarResultados();
+};
+
+#endif //CARRERA_H
